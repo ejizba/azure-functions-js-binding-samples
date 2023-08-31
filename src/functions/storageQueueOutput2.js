aa76b07325f322670f97aedbf1e711ae0c83ba7e
Eric Jizba <erijiz@microsoft.com>
2023-08-30 19:24:33 -0700

    more samples

---
+++ src/functions/storageQueueOutput2.js
@@ -0,0 +1,16 @@
+const { app, output } = require('@azure/functions');
+
+const queueOutput = output.storageQueue({
+    queueName: 'outqueue',
+    connection: 'MyStorageConnectionAppSetting',
+});
+
+app.http('httpTrigger1', {
+    methods: ['GET', 'POST'],
+    authLevel: 'anonymous',
+    extraOutputs: [queueOutput],
+    handler: async (request, context) => {
+        context.extraOutputs.set(queueOutput, ['message 1', 'message 2']);
+        return { body: 'Created queue item.' };
+    },
+});
